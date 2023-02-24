cae859760902e583e0373a7b372a2642a65a9a69
uakarsh <55104596+uakarsh@users.noreply.github.com>
2023-02-24 16:14:40 +0530

    Added the scripts for evaluation and code cleaning

---
+++ inference_utils.py
@@ -0,0 +1,84 @@
+import torch
+from torch.utils.data import TensorDataset
+
+
+def store_ids(list_of_ids):
+    ids = {}
+    current_count = 0
+    for i in range(len(list_of_ids)):
+        current_id = list_of_ids[i].split("/")[-1]
+        if current_id not in ids:
+            ids[current_id] = current_count
+            current_count += 1
+            
+    return ids
+
+
+def convert_features_to_tensor(features):
+    # Convert to Tensors and build dataset
+    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+    all_bboxes = torch.tensor([f.boxes for f in features], dtype=torch.long)
+    all_start_positions = torch.tensor([f.start_positions for f in features], dtype=torch.long) 
+    all_end_positions = torch.tensor([f.end_positions for f in features], dtype=torch.long)
+    # all_p_mask = torch.tensor([f.p_mask for f in features], dtype=torch.long)
+    # all_example_index = torch.arange(all_input_ids.size(0), dtype=torch.long)
+    
+    img_id = [f.image_id for f in features]
+    img_id = store_ids(img_id) ## Would be useful in calculating the scores
+    transform_id = torch.tensor([img_id[f.image_id.split("/")[-1]] for f in features ])
+    
+    if features[0].img is not None:
+        all_imgs = torch.stack([torch.from_numpy(x.img['pixel_values'][0]) for x in features])
+        dataset = TensorDataset(
+            all_input_ids, all_input_mask, all_segment_ids, all_start_positions, all_end_positions, all_bboxes, transform_id,
+            all_imgs, 
+        )
+        
+    else:
+        dataset = TensorDataset(
+            all_input_ids, all_input_mask, all_segment_ids, all_start_positions, all_end_positions, all_bboxes,transform_id
+        )
+    return dataset, img_id
+
+
+def decode_for_batch(start_ps, end_ps, input_ids, tokenizer):
+    decoded_statements = []
+    batch_size = len(input_ids)
+    
+    for i in range(batch_size):
+        current_id = input_ids[i]
+        sent = current_id[start_ps[i] : end_ps[i] + 1].detach().cpu().tolist()
+        sent = tokenizer.decode(sent)
+        decoded_statements.append(sent)
+        
+    return decoded_statements
+
+
+def get_pred_references(output, dl, tokenizer):
+    pred_decode_sent = decode_for_batch(start_ps = dl['start_positions'].detach().cpu(), end_ps = dl['end_positions'].detach().cpu(), 
+                                   input_ids = dl['input_ids'].detach().cpu(), tokenizer = tokenizer)
+    
+    act_decode_sent = decode_for_batch(start_ps = output.start_logits.argmax(axis = -1).detach().cpu(), end_ps = output.end_logits.argmax(axis = -1).detach().cpu(), 
+                                    input_ids = sample['input_ids'].detach().cpu(), tokenizer = tokenizer)
+    
+    
+    batch_size = len(pred_decode_sent)
+    predictions = []
+    references = []
+    
+    for i in range(batch_size):
+        unique_id = str(dl['unique_id'].item())
+        curr_pred = {"prediction_text" : act_decode_sent[i],
+                    'id' : unique_id}
+        
+        curr_ref = {"answers" : {"answer_start" : [dl['start_positions'].item()], "text" : [pred_decode_sent[i]]},
+                               'id' : unique_id}
+        
+        predictions.append(curr_pred)
+        references.append(curr_ref)
+        
+    return predictions, references
+
+
--- tokenization.py
+++ tokenization.py
@@ -1,3 +1,5 @@
+import torch
+
 def whitespace_tokenize(text):
   """Runs basic whitespace cleaning and splitting on a piece of text."""
   text = text.strip()
@@ -5,3 +7,35 @@ def whitespace_tokenize(text):
     return []
   tokens = text.split()
   return tokens
+
+
+class Collate(object):
+    
+    def __init__(self, model_name : str = "layout-lmv3_hf"):
+        self.model_name = model_name
+    
+    
+    def __call__(self, list_of_ds):
+        input_ids = torch.stack([x[0] for x in list_of_ds], axis = 0)
+        attn_mask = torch.stack([x[1] for x in list_of_ds], axis = 0)
+        token_ids = torch.stack([x[2] for x in list_of_ds], axis = 0)
+        start_ps = torch.stack([x[3] for x in list_of_ds], axis = 0)
+        end_ps = torch.stack([x[4] for x in list_of_ds], axis = 0)
+        ids = torch.stack([x[6] for x in list_of_ds], axis = 0)
+        
+        encoding = {'input_ids' : input_ids, "attention_mask" : attn_mask,
+                   "token_type_ids" : token_ids, "start_positions" : start_ps, "end_positions" : end_ps, "unique_id" : ids}
+        
+        if self.model_name in ['bert_hf', 'roberta_hf']:
+            return encoding
+        
+        else:
+            bbox = torch.stack([x[5] for x in list_of_ds], axis = 0)
+            encoding['bbox'] = bbox
+            
+            if self.model_name == "lilt_hf":
+                return encoding
+            
+            pixels = torch.stack([x[7] for x in list_of_ds], axis = 0)
+            encoding['pixel_values'] = pixels
+            return encoding
